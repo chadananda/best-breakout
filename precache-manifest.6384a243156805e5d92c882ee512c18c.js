e24a456900d00f007aa520f7e5046092705b1160
Chad <chadananda@gmail.com>
2020-07-12 22:17:45 -0700

    gitploy

---
+++ precache-manifest.6384a243156805e5d92c882ee512c18c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01036952cd3c06a6e2a34b3fa3b0933",
+    "url": "/best-breakout/index.html"
+  },
+  {
+    "revision": "d73db91341e07ffb9aa2",
+    "url": "/best-breakout/static/css/main.a421fb84.chunk.css"
+  },
+  {
+    "revision": "d73db91341e07ffb9aa2",
+    "url": "/best-breakout/static/js/main.e640be4d.chunk.js"
+  },
+  {
+    "revision": "58138d6be208074c547c",
+    "url": "/best-breakout/static/js/runtime-main.7e4c2926.js"
+  },
+  {
+    "revision": "05fd3ce5329a2268229eaf44910bf72d",
+    "url": "/best-breakout/static/media/dude.526a78a9.png"
+  },
+  {
+    "revision": "fc7d654a5cbca97fbe6091f9b37ac135",
+    "url": "/best-breakout/static/media/platform.69db09bf.png"
+  },
+  {
+    "revision": "cb8fe21064e648d4fd4aa71e2a5d339d",
+    "url": "/best-breakout/static/media/sky.47a86b19.png"
+  },
+  {
+    "revision": "3bbfad7412f062fcbae5984254093294",
+    "url": "/best-breakout/static/media/star.a7cb585c.png"
+  }
+]);
